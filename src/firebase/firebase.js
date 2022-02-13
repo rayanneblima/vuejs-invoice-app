c8202628dde43dbd167cd05794d244e8fae62e77
Rayanne B. Lima <rayanne_lima2010@hotmail.com>
2022-02-13 20:21:23 -0300

    fix: created .env for firebase variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJ_RjjUlFtWTL0NhjDBebCsfDPKZxOiz8",
-  authDomain: "invoice-app-vuejs-91434.firebaseapp.com",
-  projectId: "invoice-app-vuejs-91434",
-  storageBucket: "invoice-app-vuejs-91434.appspot.com",
-  messagingSenderId: "662240501876",
-  appId: "1:662240501876:web:8936d83fbaf3fcb9767121"
+  apiKey: process.env.VUE_APP_FIREBASE,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
 
 initializeApp(firebaseConfig);
